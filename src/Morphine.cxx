8fe019b92f05d636b80424cea6fdf14eff7ec434
Igor Bogicevic <igor@prng.org>
2019-04-18 14:10:41 +0200

    small change

--- src/Morphine.cxx
+++ src/Morphine.cxx
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "MorphineConfig.h"
+#include "MorphineInfo.h"
  
 int main (int argc, char *argv[])
 {
   fprintf(stdout,"%s Version %d.%d\n",
           argv[0],
-          Tutorial_VERSION_MAJOR,
-          Tutorial_VERSION_MINOR);
+          MORPHINE_VERSION_MAJOR,
+          MORPHINE_VERSION_MINOR);
 }
