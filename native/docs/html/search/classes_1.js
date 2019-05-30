141d728bdff6493e6b1bac04340764b07d5a7ab5
Igor Bogicevic <igor@prng.org>
2019-05-30 21:19:51 +0200

    rearrange backend parents

--- native/docs/html/search/classes_1.js
+++ native/docs/html/search/classes_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['backend',['Backend',['../classhomomorphine_1_1_backend.html',1,'homomorphine']]],
   ['backendexception',['BackendException',['../classhomomorphine_1_1_backend_exception.html',1,'homomorphine']]],
   ['backendfactoryexception',['BackendFactoryException',['../classhomomorphine_1_1_backend_factory_exception.html',1,'homomorphine']]],
   ['backendoperationnotsupported',['BackendOperationNotSupported',['../classhomomorphine_1_1_backend_operation_not_supported.html',1,'homomorphine']]]
---
+++ native/src/boolean_circuit_backend.hpp
@@ -0,0 +1,109 @@
+#ifndef _BOOLEAN_CIRCUIT_HOMOMORPHINE_BACKEND_H_
+#define _BOOLEAN_CIRCUIT_HOMOMORPHINE_BACKEND_H_
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "backend.hpp"
+
+using namespace std;
+
+namespace homomorphine 
+{
+  /*! /brief Backend interface
+   * 
+   * Provides an interface that each specific arithmetic backend has to provide
+   */
+  class ArithmeticBackend : public Backend
+  {
+    protected:
+      map<string, string> params; /*!< params that are consumed by specific implementations of the arithmetic homomorphic backend */
+
+    public:
+
+      /*! 
+       * Backend cleanup
+       */
+      virtual ~ArithmeticBackend() { this->params.clear(); };
+
+      /*! 
+       * Sets the specific homomorphic algorithm implementation
+       * that backend provides
+       * 
+       * \param algorithm homomorphic encryption algorithm
+       */
+      virtual void setAlgorithm(string algorithm) = 0;
+
+      /*!
+       * Backend initialization method - initializes all the basic
+       * parameters and behavior of a specific backend implementation
+       */
+      virtual void init() = 0;
+
+      /*!
+       * Generates the public/secret key pair
+       */
+      virtual void generateKeys() = 0;
+
+      /*!
+       * Returns the UUEncoded public key
+       * 
+       * \return public key
+       */
+      virtual string getPublicKey() = 0;
+
+      /*!
+       * Returns the UUEncoded secret key
+       * 
+       * \return secret key
+       */
+      virtual string getSecretKey() = 0;
+
+      /*!
+       * Returns the pair of UUEncoded public and secret keys
+       * 
+       * \return pair of public and secret keys
+       */
+      virtual pair<string, string> getKeys() = 0; 
+
+      /*!
+       * Sets the public key 
+       * 
+       * \param public_key UUEncoded public key
+       */
+      virtual void setPublicKey(string public_key) = 0;
+
+      /*!
+       * Sets the secret key 
+       * 
+       * \param secret_key UUEncoded secret key
+       */
+      virtual void setSecretKey(string secret_key) = 0;
+
+      /*!
+       * Sets the both public and secret keys 
+       * 
+       * \param public_key UUEncoded public key
+       * \param secret_key UUEncoded secret key
+       */
+      virtual void setKeys(string public_key, string secret_key) = 0;
+
+      /*!
+       * Returns the UUEncoded cipher containing ecrypted value, or vector of values
+       * 
+       * \return UUEncoded cipher
+       */
+      virtual string getCipher() = 0;
+
+      /*!
+       * Sets the UUEncoded cipher containing ecrypted value, or vector of values
+       * 
+       * \param cipher UUEncoded cipher
+       */
+      virtual void setCipher(string cipher) = 0;
+  };
+  
+}
+
+#endif
